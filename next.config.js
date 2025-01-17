d800a07da3ffdbc11f428859b511a0bc37413a0c
EZ3ki33l <r.rousset31@gmail.com>
2025-01-17 19:30:10 +0100

    test commit

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'media.castorama.fr',
+      'cdn.manomano.com',
+      'www.leroymerlin.fr',
+      'img.archiexpo.fr',
+      'www.bricodepot.fr',
+      'www.bricorama.fr',
+      'www.amazon.fr',
+      'images.fr.shopping.rakuten.com',
+      'm.media-amazon.com',
+      'www.cdiscount.com'
+    ],
+  },
+}
+
+module.exports = nextConfig 
