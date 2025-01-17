d52f2eb738944fd1c18e3003181cb48f92c9c255
EZ3ki33l <r.rousset31@gmail.com>
2025-01-17 18:38:03 +0100

    test commit

--- app/admin/create/page.tsx
+++ app/admin/create/page.tsx
@@ -300,7 +300,7 @@ export default function AdminPage() {
               onClick={handleAddManualImage}
               className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
             >
-              Ajouter l'image
+              Ajouter l&apos;image
             </button>
           </div>
           <div
